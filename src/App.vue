7b7d7876d1faa91a4b32d60a90a969c823a378f2
Philippe Fonzin <pfonzin@gmail.com>
2020-06-08 19:41:54 -0400

    Play / pause methods

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,18 @@
           <!-- "v-if" is a vue conditional used to conditionally render a block. 
           The block will only be rendered if the directive’s expression returns 
           a truthy value -->
-          <button class="prev" v-if="!isPlaying">Prev</button>
-          <button class="play" v-else>Play</button>
-          <button class="pause">Pause</button>
+          <button class="prev">Prev</button>
+          <button class="play" v-if="!isPlaying" @click="play">Play</button>
+          <button class="pause" v-else @click="pause">Pause</button>
           <button class="next">Next</button>
         </div>
+      </section>
+      <section class="playlist">
+        <h3>The Playlist</h3>
+        <button v-for="song in songs" :key="song.src" @click=play(song) 
+        :class="(song.src == current.src) ? 'song playing' : 'song'">
+          {{song.title}} - {{song.artist}}
+        </button>
       </section>
     </main>
   </div>
@@ -81,12 +88,29 @@ export default {
           src: require('./assets/09_Double_07.mp3')
         },
         {
-          title: 'Success_Is_My_Cologne',
+          title: 'Success Is My Cologne',
           artist: 'Curren$y',
           src: require('./assets/10_Success_Is_My_Cologne.mp3')
         }
       ],
       player: new Audio()
+    }
+  },
+
+  methods: {
+    play(song) {
+      if (typeof song.src != 'undefined') {
+        this.current = song;
+
+        this.player.src = this.current.src;
+      }
+
+      this.player.play();
+      this.isPlaying = true;
+    },
+    pause() {
+      this.player.pause();
+      this.isPlaying = false;
     }
   },
   // This is a lifecycle method. As soon as the app gets created this method is run
