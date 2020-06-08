fcf84688da369e0874046dd38f32aa9a29efcac8
Philippe Fonzin <pfonzin@gmail.com>
2020-06-08 17:59:20 -0400

    Create and style header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,48 @@
 <template>
-
   <div id="app">
-
+    <header>
+      <h1>My Music</h1>
+    </header>
+    <main>
+      <section class="player">
+        <h2 class="song-title">Test</h2>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+    // the "data" returns our state
+  data () {
+    return {
+      current: {
+        title: 'SONG TITLE'
+      }
+    }
+  }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: sans-serif; 
+}
+
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  background-color: #212121;
+  color: #FFF; 
+}
 
 </style>
